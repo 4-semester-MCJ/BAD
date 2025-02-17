7594fc0720bf75a66adde945f20b80fdbd428f72
Bananasplit393 <202305666@post.au.dk>
2025-02-17 23:31:38 +0100

    Done Diego

--- CREATE.sql
+++ CREATE.sql
@@ -1,11 +1,51 @@
-CREATE DATABASE experienceSharedDb;
-GO
-
+-- CREATE DATABASE experienceSharedDb;
+-- GO
 USE experienceSharedDb;
-CREATE TABLE Guests
-(
-    GuestId INT PRIMARY KEY,
-    GuestName NVARCHAR(50),
-    GuestPhoneNumber NVARCHAR(50),
-    GuestAge INT
+
+CREATE TABLE Providers (
+    ProvId INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(255) NOT NULL,
+    BPA NVARCHAR(255) NOT NULL,
+    PhoneNum NVARCHAR(20) NOT NULL,
+    CVR NVARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE Guests (
+    GId INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(255) NOT NULL,
+    Age INT NOT NULL,
+    PhoneNum NVARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE Experiences (
+    EId INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(255) NOT NULL,
+    Description TEXT,
+    ProvId INT NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (ProvId) REFERENCES Providers(ProvId)
+);
+
+CREATE TABLE SE (
+    SEId INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(255) NOT NULL,
+    Date DATETIME NOT NULL,
+);
+
+-- Table linking Shared Experiences with Experiences
+CREATE TABLE SEDets (
+    SEId INT NOT NULL,
+    EId INT NOT NULL,
+    PRIMARY KEY (SEId, EId),
+    FOREIGN KEY (SEId) REFERENCES SE(SEId),
+    FOREIGN KEY (EId) REFERENCES Experiences(EId)
+);
+
+-- Table for Guests joining Shared Experiences
+CREATE TABLE SEGuests (
+    SEId INT NOT NULL,
+    GId INT NOT NULL,
+    PRIMARY KEY (SEId, GId),
+    FOREIGN KEY (SEId) REFERENCES SE(SEId),
+    FOREIGN KEY (GId) REFERENCES Guests(GId)
 );
