22067a9ca2be715fa307ed51840eabe2bcba7093
Michael Dao <202307644@post.au.dk>
2025-03-19 13:50:44 +0100

    Migrations 1, 2 og 3

---
+++ BAD_MA2_Solution_grp14/Migrations/20250318093536_Migration1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BAD_MA2_Solution_grp14.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    GuestId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.GuestId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Providers",
+                columns: table => new
+                {
+                    ProviderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BuisnessPhysicalAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CVR = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TouristicOperatorPermitPdf = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providers", x => x.ProviderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SharedExperiences",
+                columns: table => new
+                {
+                    SharedExperienceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedExperiences", x => x.SharedExperienceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Experiences",
+                columns: table => new
+                {
+                    ExperienceId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProviderId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Experiences", x => x.ExperienceId);
+                    table.ForeignKey(
+                        name: "FK_Experiences_Providers_ProviderId",
+                        column: x => x.ProviderId,
+                        principalTable: "Providers",
+                        principalColumn: "ProviderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SharedExperienceGuests",
+                columns: table => new
+                {
+                    SharedExperienceId = table.Column<int>(type: "int", nullable: false),
+                    GuestId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedExperienceGuests", x => new { x.SharedExperienceId, x.GuestId });
+                    table.ForeignKey(
+                        name: "FK_SharedExperienceGuests_Guests_GuestId",
+                        column: x => x.GuestId,
+                        principalTable: "Guests",
+                        principalColumn: "GuestId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SharedExperienceGuests_SharedExperiences_SharedExperienceId",
+                        column: x => x.SharedExperienceId,
+                        principalTable: "SharedExperiences",
+                        principalColumn: "SharedExperienceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SharedExperienceDetails",
+                columns: table => new
+                {
+                    SharedExperienceId = table.Column<int>(type: "int", nullable: false),
+                    ExperienceId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SharedExperienceDetails", x => new { x.SharedExperienceId, x.ExperienceId });
+                    table.ForeignKey(
+                        name: "FK_SharedExperienceDetails_Experiences_ExperienceId",
+                        column: x => x.ExperienceId,
+                        principalTable: "Experiences",
+                        principalColumn: "ExperienceId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SharedExperienceDetails_SharedExperiences_SharedExperienceId",
+                        column: x => x.SharedExperienceId,
+                        principalTable: "SharedExperiences",
+                        principalColumn: "SharedExperienceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Experiences_ProviderId",
+                table: "Experiences",
+                column: "ProviderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedExperienceDetails_ExperienceId",
+                table: "SharedExperienceDetails",
+                column: "ExperienceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedExperienceGuests_GuestId",
+                table: "SharedExperienceGuests",
+                column: "GuestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SharedExperienceDetails");
+
+            migrationBuilder.DropTable(
+                name: "SharedExperienceGuests");
+
+            migrationBuilder.DropTable(
+                name: "Experiences");
+
+            migrationBuilder.DropTable(
+                name: "Guests");
+
+            migrationBuilder.DropTable(
+                name: "SharedExperiences");
+
+            migrationBuilder.DropTable(
+                name: "Providers");
+        }
+    }
+}
